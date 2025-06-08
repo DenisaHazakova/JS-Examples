4ea388b31ab193730aa2163e4901c72f342adc5d
Denisa Hazakova <135653277+DenisaHazakova@users.noreply.github.com>
2025-06-08 10:18:06 +0200

    Update script.js
    added notes to lines of code

--- Table/script.js
+++ Table/script.js
@@ -1,3 +1,4 @@
+// Creating array of objects named cities with parameters id, name, area, location
 const cities = [
   {
     id: 1001,
@@ -71,103 +72,123 @@ const cities = [
   },
 ];
 
+// Declare global variable named tableColumns as array to use these values later in this file
 const tableColumns = ["name", "area", "population", "location"];
+
+// Create variable as let because it will change later in this code. It is declared as global one so we can
+// use it later
 let selectedRowCityIndex = null;
 
+// With DOMContent Loaded we ensure that when DOM is loaded, then function renderCities is called
 document.addEventListener("DOMContentLoaded", (event) => {
   renderCities();
 });
 
 function renderCities() {
-  //declare of element table body
+  // Declare of element table body
   const tbody = document.getElementById("tableBody");
 
-  //loop through cities
+  // Loop through cities from index 0
   for (let index = 0; index < cities.length; index++) {
     const city = cities[index];
-    //create row element
+    
+    // Create row element
     const row = document.createElement("tr");
+    // Insert atribute to row so we can later use index of current row
     row.setAttribute("data-index", index);
 
-    //append it to its parent table body
+    // Append it to its parent - table body
     tbody.appendChild(row);
 
-    //loop through tableColumns
+    // Loop through tableColumns
     for (const column of tableColumns) {
-      //create column el
+      // Create column el
       const columnEl = document.createElement("td");
-
+      // Set atribute to column element so we can use name of column later
       columnEl.setAttribute("data-key", column);
-      //insert value to columnEl
+      // Insert value to columnEl
       columnEl.innerHTML = city[column];
 
-      //append columnEl to its parent
+      // Append columnEl to its parent
       row.appendChild(columnEl);
     }
 
-    //create column for action
+    // Create last column for action
     const actColEl = document.createElement("td");
 
-    //create button element and name it show detail
+    // Create button element and name it 'show detail'
     const btn = document.createElement("button");
     btn.textContent = "Show detail";
 
-    //assign data-id with id of city
+    // Assign data-id atribute with id of city
     btn.setAttribute("data-id", city.id);
 
-    //append btn to its parent
+    // Append btn to its parent
     actColEl.appendChild(btn);
 
-    //append it to its parent
+    // Append it to its parent
     row.appendChild(actColEl);
 
-    //function to call when click on btn
+    // Function to call when click on btn
     btn.addEventListener("click", onRowClick);
   }
 }
 
-// clicking on button show detail
+// Clicking on button show detail
 function onRowClick(event) {
-  //find id of button
+  // Find id of button
   const rowId = event.target.getAttribute("data-id");
-  //check if button id is the same as city id
+  // Check if button id is the same as city id
   selectedRowCityIndex = cities.findIndex((city) => city.id == rowId);
   const foundedCity = cities[selectedRowCityIndex];
 
-  //declare parent element div
+  // Declare parent element div including all inputs
   const parentEl = document.getElementById("cityDetail");
+
+  // We want to show the div, so we need to remove the class which ensures that div is hidden
+  // After that parentEl will be shown on web
   parentEl.classList.remove("hidden");
 
-  // declaration of all inputs
+  // Declaration of all inputs
   const cityInput = document.getElementById("cityName");
   const areaInput = document.getElementById("area");
   const populationInput = document.getElementById("population");
   const locationInput = document.getElementById("location");
 
-  // insert values from object foundedCity into each input
+  // Insert values from object foundedCity into each input
   cityInput.value = foundedCity.name;
   areaInput.value = foundedCity.area;
   populationInput.value = foundedCity.population;
   locationInput.value = foundedCity.location;
-  //hodnota kazdeho inputu/ zadeklarovat si input cez getelement byid / potom input.value = foundedObject.name
-  ////inputy pod parentom div s classou hidden
-  //
 }
 
+// We want to ensure that changes in array of objects cities will be updates/shown also in the table on web
 function onUpdate() {
+  // Declare the object updatedCity with the same attributes as cities has and assign the currentvalues of inputs
   const updatedCity = {
     name: document.getElementById("cityName").value,
     area: document.getElementById("area").value,
     location: document.getElementById("location").value,
     population: document.getElementById("population").value,
   };
+
+  // selectedRowCityIndex is the index of clicked row
+  // cities[selectedRowCityIndex] = result is object currently clicked
+  // we assign value of clicked object to object details filled in inputs
   cities[selectedRowCityIndex] = updatedCity;
 
+  // Declare element table body
   const tbody = document.getElementById("tableBody");
+  // Declare row currently clicked in main table cities
   const trEl = tbody.querySelector(`tr[data-index="${selectedRowCityIndex}"]`);
 
+  // Loop through array tableColumns
   for (const column of tableColumns) {
+    // Declare the column element
+    // With querySelector we find attribute of column with the same value as it is currently the value of const column in loop
+    // With that we ensure that changed value in input will be reflected in correct column in main table columns
     const tdElement = trEl.querySelector(`td[data-key=${column}]`);
+    // Into founded column we insert that value which is inserted in inputs
     tdElement.textContent = updatedCity[column];
   }
 }
